acff459bdcfb5e0af66d82299f579447281345ab
Irsa Ayaz <irsaayaz.525@gmail.com>
2025-05-15 12:36:16 +0500

    Add files via upload

---
+++ data_types_assignments.py
@@ -0,0 +1,13 @@
+#Task 1: Integer
+#age = 11
+#print("In five years, you will be", age + 5)
+
+#Task 2: String
+#name = "Irsa"
+#greeting = "Hello, " + name + "!"
+#print(greeting)
+
+#Task 3: Float
+height = 15.67
+text = "Your height is, 15.67 meters"
+print(text)
